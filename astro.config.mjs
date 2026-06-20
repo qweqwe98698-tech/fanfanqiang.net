1cd9bde74a18056c3a6ec1403b997285c8d1a5a4
Admin <admin@clashvpns.cloud>
2026-06-20 13:02:21 +0800

    Set build format to file for SEO compatibility

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,5 +5,8 @@ import sitemap from '@astrojs/sitemap';
 // https://astro.build/config
 export default defineConfig({
   site: 'https://fanfanqiang.net',
+  build: {
+    format: 'file'
+  },
   integrations: [sitemap()],
 });
